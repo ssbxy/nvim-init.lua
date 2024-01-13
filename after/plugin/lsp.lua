023afaad54297ea37f62a79e81f5bdcb902da732
Samuel Burrows <sam.burrows@rocketmail.com>
2024-01-13 19:29:45 +0800

    Added jedi_language_server

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,8 +1,9 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls", "clangd", "tsserver", "rust_analyzer" }
+  ensure_installed = { "lua_ls", "clangd", "tsserver", "rust_analyzer", "jedi_language_server" }
 })
 require("lspconfig").lua_ls.setup {}
 require("lspconfig").clangd.setup {}
 require("lspconfig").tsserver.setup {}
 require("lspconfig").rust_analyzer.setup {}
+require("lspconfig").jedi_language_server.setup {}
