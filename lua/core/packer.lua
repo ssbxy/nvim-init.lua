759add16061ced62f85e8a8efbcb822c506fd1e4
Samuel Burrows <sam.burrows@rocketmail.com>
2024-01-14 18:50:37 +0800

    Added dependencies

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -5,15 +5,22 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'airblade/vim-gitgutter'
   use 'shaunsingh/nord.nvim'
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'L3MON4D3/LuaSnip'
-  use{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'}
-  use 'nvim-lua/plenary.nvim'
-  use{'nvim-telescope/telescope.nvim', tag = '0.1.5'}
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    requires = {
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+      {'neovim/nvim-lspconfig'},
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'L3MON4D3/LuaSnip'},
+    }
+  }
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
   use{'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
 
 end)
